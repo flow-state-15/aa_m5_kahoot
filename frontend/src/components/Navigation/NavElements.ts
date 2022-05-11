1db2ed505ac4cef4dea1c5abe39a91c7106f9b48
Nathan Treadaway <natetread@gmail.com>
2022-05-11 16:10:11 -0400

    setting up basic navbar

--- frontend/src/components/Navigation/NavElements.ts
+++ frontend/src/components/Navigation/NavElements.ts
@@ -1,8 +1,8 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import { Link } from '@mui/material'
-import styled from '@mui/styled-engine'
+import CreateStyled from '@mui/styled-engine'
 
-export const Nav = styled.nav`
+export const Nav = styled.div`
     background: #000;
     height: 80px;
     margin-top: -80px;
@@ -30,7 +30,7 @@ export const NavbarContainer = styled.div`
     max-width: 1800px;
 `
 
-export const NavLogo = styled(Link)`
+export const NavLogo = CreateStyled(Link)`
     color: #00FF00;
     cursor: pointer;
     font-size: 1.5rem;
@@ -39,4 +39,73 @@ export const NavLogo = styled(Link)`
     margin-left: 20px;
     font-weight: bold;
     text-decoration: none;
+`
+export const MobileIcon = styled.div`
+    display: none;
+
+    @media screen and (max-width: 760px){
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-100%, 60%);
+        font-size: 1.8rem;
+        cursor: pointer;
+        color: #fff;
+    }
+`
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: cneter;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 761px){
+        display: none;
+    }
+`
+export const NavItem = styled.ul`
+    height: 80px
+`
+export const NavLinks = CreateStyled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active{
+        border-bottom: 3px solid #01bf71;
+    }
+`
+export const NavBtn = styled.nav`
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 760px){
+        display: none;
+    }
+`
+
+export const NavBtnLink = CreateStyled(Link)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover{
+        transition: all 0.2s ease-in-and-out;
+        background: #fff;
+        color: #010606;
+    }
 `
--- frontend/src/components/Navigation/index.tsx
+++ frontend/src/components/Navigation/index.tsx
@@ -1,13 +1,53 @@
 // import { Link } from 'react-router-dom'
-import { NavLogo, Nav, NavbarContainer } from './NavElements';
+import { FaBars } from 'react-icons/fa'
+import {
+    NavLogo,
+    Nav,
+    NavbarContainer,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavElements';
+
 // import { Link, Button } from '@mui/material';
 
 export default function Navigation() {
+
+    const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        const button: HTMLButtonElement = event.currentTarget;
+        setClickedButton(button.name);
+      };
+
     return (
         <>
             <Nav>
                 <NavbarContainer>
-
+                    <NavLogo href='/'>Logo</NavLogo>
+                    <MobileIcon onClick={buttonHandler}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks href="/about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks href="/discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks href="/service">Service</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks href="/signup">Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink href="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
                 </NavbarContainer>
             </Nav>
         </>
