af25bc345df153b39f969b5f7811e6da6452de0a
Daniel Flores <daniel.flores4930@gmail.com>
2022-05-11 12:54:58 -0400

    got basic react router v6 working

--- frontend/src/components/Course/index.js
+++ frontend/src/components/Course/index.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Folders from '../Folders';
+
 export default function Course() {
     return (
-        <div className="course">
-            <h2>This is the Course Component</h2>
-        </div>
+        <>
+            <div className="course">
+                <h2>This is the Course Component</h2>
+            </div>
+            <Routes>
+                <Route path="/folders" element={<Folders />} />
+            </Routes>
+        </>
     );
 }
