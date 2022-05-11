cc4c514c1e48c56ef4a012aa412ae7ac3fad3a0f
Nathan Treadaway <natetread@gmail.com>
2022-05-11 13:12:29 -0400

    added /* for library nest routes

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "@types/react-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 
 export default function Library() {
@@ -12,26 +12,28 @@ export default function Library() {
 
     return (
         <div className="library">
+
+            {/* <Routes>
+                <Route path="/all" element={<Recent />} />
+                <Route path="/drafts" element={<Drafts />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/shared" element={<Shared />} />
+            </Routes> */}
+
             {/* Recent shows all in the URL on Kahoot */}
-            <Link to='/all'> Recent</Link>
-            <Link to='/drafts'> Drafts</Link>
-            <Link to='/favorites'> Favorites</Link>
-            <Link to='/shared'> Shared with me</Link>
+            <Link to='all'> Recent</Link>
+            <Link to='drafts'> Drafts</Link>
+            <Link to='favorites'> Favorites</Link>
+            <Link to='shared'> Shared with me</Link>
             <input
                 type="text"
                 placeholder="Search"
                 onChange={handleChange}
                 value={searchInput} />
+
             <div>
                 <h1>Content will go here based on the link chosen</h1>
             </div>
         </div>
     );
-
-    <Routes>
-        <Route path="/all" element={<Recent />} />
-        <Route path="/drafts" element={<Recent />} />
-        <Route path="/favorites" element={<Recent />} />
-        <Route path="/shared" element={<Recent />} />
-    </Routes>
 }
