bda0defe9420159de705d4cdd0ac392bafbfb536
flow-state-15 <purcellgroup88@gmail.com>
2022-05-11 12:02:09 -0500

    in progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>APP ROOT</h1>
+
     </>
   );
 }
--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,4 +1,10 @@
+import React from "react";
+
 export default function Home() {
+    const [test, setTest] = React.useState();
+
+    
+
     return (
         <div className="home">
             <h2>This is the Home Component</h2>
