1db6cb126702e9c5a5eaf4bafd9055d1b20f10b6
Nathan Treadaway <natetread@gmail.com>
2022-05-11 13:03:26 -0400

    added nested routes for library

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -1,7 +1,37 @@
+import { Link, Route, Routes } from "@types/react-dom";
+import { useState } from "react";
+
 export default function Library() {
+
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+
     return (
         <div className="library">
-            <h2>This is the Library Component</h2>
+            {/* Recent shows all in the URL on Kahoot */}
+            <Link to='/all'> Recent</Link>
+            <Link to='/drafts'> Drafts</Link>
+            <Link to='/favorites'> Favorites</Link>
+            <Link to='/shared'> Shared with me</Link>
+            <input
+                type="text"
+                placeholder="Search"
+                onChange={handleChange}
+                value={searchInput} />
+            <div>
+                <h1>Content will go here based on the link chosen</h1>
+            </div>
         </div>
     );
+
+    <Routes>
+        <Route path="/all" element={<Recent />} />
+        <Route path="/drafts" element={<Recent />} />
+        <Route path="/favorites" element={<Recent />} />
+        <Route path="/shared" element={<Recent />} />
+    </Routes>
 }
