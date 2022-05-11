492c7485f517ca55db7568f59aa426dfe34997fe
flow-state-15 <purcellgroup88@gmail.com>
2022-05-11 13:18:51 -0500

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Navigation from './components/Navigation';
 import Library from './components/Library';
-import CounterLogic from './components/features/counter/Counter';
 import CreateQuiz from './components/CreateQuiz';
+import Counter from './components/features/counter';
 import './App.css';
 
 function App() {
@@ -21,7 +19,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/library/*" element={<Library />} />
-          <Route path="/counter" element={<CounterLogic />} />
+          <Route path="/counter" element={<Counter />} />
           <Route path="/createquiz" element={<CreateQuiz />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-export default function Home() {
-    const [test, setTest] = React.useState();
-
-    
-
+export default function Home(): JSX.Element {
     return (
-        <div className="home">
+        <div className="home-root">
             <h2>This is the Home Component</h2>
+            <div>Mod 1</div>
+            <div>Mod 2</div>
+            <div>Mod 3</div>
+            <div>Mod 4</div>
+            <div>Mod 5</div>
+            <div>Mod 6</div>
+            <div>Mod 7</div>
+            <div><h2>How to use</h2></div>
         </div>
     );
 }
