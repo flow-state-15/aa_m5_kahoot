22b92791a4710b1c2ac9a3face8dc65217205d72
Daniel Flores <daniel.flores4930@gmail.com>
2022-05-12 12:31:45 -0400

    some touches to styling

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -6,11 +6,11 @@ const LoginButton = styled('button')({
     borderRadius: 4,
     fontFamily: 'Montserrat, sans-serif',
     minWidth: '360px',
+    minHeight: '42px',
     display: "flex", 
     justifyContent: "center",
     backgroundColor: "white",
     fontSize: "0.875rem",
-
   });
 
 const LoginDiv = styled('div')({
@@ -18,7 +18,15 @@ const LoginDiv = styled('div')({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100vh',
+    height: '37rem',
+    width: '25rem',
+    border: '1px solid #e0e0e0',
+    position: 'relative',
+    top: '0vh',
+    left: '35vw',
+    borderRadius: '5px',
+    boxShadow: '0px 0px 10px black',
+    backgroundColor: 'white',
 });
 
 const PretendForm = styled('form')({
@@ -29,18 +37,26 @@ const PretendForm = styled('form')({
 const GoogleLogo = styled('img')({
     position: 'absolute',
     height: '1.5rem',
-    left: '28.5rem',
-    top: '21.75rem',
+    left: '3vw',
+    top: '48.75vh',
 });
+
+const BackgroundDiv = styled('div')({
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'lightgray',
+})
 
 export default function Login() {
     return (
-        <LoginDiv className="login">
-            <h2 className="login-title">Log In</h2>
-            <PretendForm className="login-form">
-                <LoginButton className="login-button-dummy">Continue with Google</LoginButton>
+        <BackgroundDiv>
+            <LoginDiv className="login">
+                <h2 className="login-title">Log In</h2>
+                <PretendForm className="login-form">
+                    <LoginButton className="login-button-dummy">Continue with Google</LoginButton>
+                </PretendForm>
                 <GoogleLogo src={googleLogo}/>
-            </PretendForm>
-        </LoginDiv>
+            </LoginDiv>
+        </BackgroundDiv>
     );
 }
