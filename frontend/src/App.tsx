7542efe5919b47f7fc7696ac5c130ac6c1ca551d
flow-state-15 <purcellgroup88@gmail.com>
2022-05-12 12:18:07 -0500

    auth0 app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,24 +7,31 @@ import Library from './components/Library';
 import CreateQuiz from './components/CreateQuiz';
 import Counter from './components/features/counter';
 import SideNavBar from './components/SideNavbar/SideNavBar';
+import { Auth0Provider } from "@auth0/auth0-react";
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>APP ROOT</h1>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/library/*" element={<Library />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/createquiz" element={<CreateQuiz />} />
-          <Route path="/sidenavbar" element={<SideNavBar />} />
-        </Routes>
-      </BrowserRouter>
+      <Auth0Provider
+      domain="dev-l4rxqvn7.us.auth0.com"
+      clientId="8S7nJ7izTPC4HbPP6uXAYQSehpZJQu6L"
+      redirectUri={window.location.origin}
+      >
+        <h1>APP ROOT</h1>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/library/*" element={<Library />} />
+            <Route path="/counter" element={<Counter />} />
+            <Route path="/createquiz" element={<CreateQuiz />} />
+            <Route path="/sidenavbar" element={<SideNavBar />} />
+          </Routes>
+        </BrowserRouter>
+      </Auth0Provider>,
     </>
   );
 }
