ecef141d33c4d86a06eb4b4036f926ff7e193b70
Daniel Flores <daniel.flores4930@gmail.com>
2022-05-12 13:46:01 -0400

    added more styling to add buttons

--- frontend/src/components/CreateQuiz/index.tsx
+++ frontend/src/components/CreateQuiz/index.tsx
@@ -8,7 +8,6 @@ const BackgroundDiv = styled('div')({
 
 const StyledInput = styled('input')({
     width: '95%',
-    height: '46px',
     textAlign: 'center',
     fontFamily: 'Montserrat, sans-serif',
     borderRadius: '5px',
@@ -19,10 +18,11 @@ const ContainerDiv = styled('div')({
     gridTemplateColumns: '1fr 1fr',
     gridTemplateRows: '1fr 1fr',
     position: 'absolute',
-    bottom: '5vh',
+    bottom: '15vh',
     width: '95vw',
     height: '10rem',
-    left: "2.5vw"
+    left: "2.5vw",
+    rowGap: '1rem',
 })
 
 export default function CreateQuiz() {
@@ -32,35 +32,35 @@ export default function CreateQuiz() {
                 placeholder='Start typing your question' 
                 onFocus={e => e.target.placeholder = ''} 
                 onBlur={e => e.target.placeholder = 'Start typing your question'}
-                sx={{position: 'absolute', top: '25vh', left: '2.5vw', fontSize: '1.25rem',}}
+                sx={{position: 'absolute', top: '25vh', left: '2.5vw', fontSize: '1.25rem', height: '2.5rem'}}
             />
             <ContainerDiv>
                 <StyledInput 
                     placeholder='Add answer 1' 
                     onFocus={e => e.target.placeholder = ''} 
                     onBlur={e => e.target.placeholder = 'Add answer 1'}
-                    sx={{margin: 'auto'}}
+                    sx={{margin: 'auto', height: '6.375rem'}}
                 />
                 
                 <StyledInput 
                     placeholder='Add answer 2' 
                     onFocus={e => e.target.placeholder = ''} 
                     onBlur={e => e.target.placeholder = 'Add answer 2'}
-                    sx={{margin: 'auto'}}
+                    sx={{margin: 'auto', height: '6.375rem'}}
                 />
                 
                 <StyledInput 
                     placeholder='Add answer 3 (optional)' 
                     onFocus={e => e.target.placeholder = ''} 
                     onBlur={e => e.target.placeholder = 'Add answer 3 (optional)'}
-                    sx={{margin: 'auto'}}
+                    sx={{margin: 'auto', height: '6.375rem'}}
                 />
                 
                 <StyledInput 
                     placeholder='Add answer 4 (optional)' 
                     onFocus={e => e.target.placeholder = ''} 
                     onBlur={e => e.target.placeholder = 'Add answer 4 (optional)'}
-                    sx={{margin: 'auto'}}
+                    sx={{margin: 'auto', height: '6.375rem'}}
                 />
             </ContainerDiv>
         </BackgroundDiv>
